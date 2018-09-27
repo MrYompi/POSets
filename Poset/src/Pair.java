6907547287dc5174aa9f8cc63c794cd5a7582d6e
MrYompi <Ojedamorala@hotmail.com>
2018-09-28 00:34:11 +0200

    Add files via upload

---
+++ Poset/src/Pair.java
@@ -0,0 +1,42 @@
+
+public class Pair {
+	private int x, y;
+
+	public Pair(int a, int b) {
+		x = a;
+		y = b;
+	}
+
+	public int x() {
+		return x;
+	}
+
+	public int y() {
+		return y;
+	}
+
+	public void setX(int a) {
+		x = a;
+	}
+
+	public void setY(int a) {
+		y = a;
+	}
+
+	public int hashCode() {
+		return 7*x+13*y;
+	}
+	
+	public boolean equals(Object o) {
+		if (o instanceof Pair) {
+			Pair q=(Pair) o;
+			return this.x==q.x() && this.y==q.y();
+		} else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		return "("+x+","+y+")";
+	}
+}
---
+++ Poset/src/Verificar.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Verificar {
+	private static Set<Pair> s;
+
+	public static void main(String[] args) {
+		s = new HashSet<Pair>();
+		s.add(new Pair(1, 1));
+		s.add(new Pair(2, 2));
+		s.add(new Pair(3, 3));
+		s.add(new Pair(1, 2));
+		s.add(new Pair(2, 3));
+		s.add(new Pair(1, 3));
+		System.out.println(verify());
+	}
+
+	private static boolean verify() {
+		boolean orden = true;
+		// aux es el conjunto sobre el que se aplica la relacion
+		Set<Integer> aux = new HashSet<Integer>();
+		for (Pair p : s) {
+			aux.add(p.x());
+			aux.add(p.y());
+		}
+		// reflexiva
+		for (int o : aux) {
+			orden = s.contains(new Pair(o, o)) ? orden : false;
+		}
+		// transitiva
+		for (Pair p : s) {
+			int o = p.x();
+			for (Pair q : s) {
+				if (!q.equals(p) && q.x() == p.y()) {
+					orden = s.contains(new Pair(o, q.y())) ? orden : false;
+				}
+			}
+		}
+		// antisimetrica
+		for (Pair p : s) {
+			if (p.x() != p.y()) {
+				orden = s.contains(new Pair(p.y(), p.x())) ? false : orden;
+			}
+		}
+		return orden;
+	}
+}
